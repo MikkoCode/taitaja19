ec1160c178a86ad361b2ec13ddeff62efb3da076
Mikko <yashoo_11@hotmail.fi>
2019-01-01 15:26:50 +0200

    Kaaviot

--- public/js/kaaviot.js
+++ public/js/kaaviot.js
@@ -48,6 +48,8 @@ function initializeJSON(){
         }
     }
 
+    labelArray.sortBy(function(o){ return new Date(o) });
+
     createChart();
 
 }
@@ -106,4 +108,27 @@ function createChart(){
 
 }
 
+(function(){
+    if (typeof Object.defineProperty === 'function'){
+        try{Object.defineProperty(Array.prototype,'sortBy',{value:sb}); }catch(e){}
+    }
+    if (!Array.prototype.sortBy) Array.prototype.sortBy = sb;
+
+    function sb(f){
+        for (var i=this.length;i;){
+            var o = this[--i];
+            this[i] = [].concat(f.call(o,o,i),o);
+        }
+        this.sort(function(a,b){
+            for (var i=0,len=a.length;i<len;++i){
+                if (a[i]!=b[i]) return a[i]<b[i]?-1:1;
+            }
+            return 0;
+        });
+        for (var i=this.length;i;){
+            this[--i]=this[i][this[i].length-1];
+        }
+        return this;
+    }
+})();
 
